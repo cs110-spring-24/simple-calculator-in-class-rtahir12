559f8dd90a7916cb6814541bc8da31a8b396cfbd
rtahir12 <157747924+rtahir12@users.noreply.github.com>
2024-02-07 17:36:27 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,27 @@
 
+ input("Enter your number 1: ")
+    number1 = (number1input)
+
+operator = input("Enter your operator: ")
+
+number2input = input("Enter your num 2: ")
+
+if operator == '+' and num1 is not None and num2 is not None:
+    result = num1 + num2
+elif operator == '-' and num1 is not None and num2 is not None:
+    result = num1 - num2
+elif operator == '*' and num1 is not None and num2 is not None:
+    result = num1 * num2
+elif operator == '/' and num1 is not None and num2 is not None:
+        result = num1 / num2
+    
+elif operator == '**' and num1 is not None and num2 is not None:
+    result = num1 ** num2
+elif operator == '//' and num1 is not None and num2 is not None:
+    result = num1 // num2
+elif operator == '%' and num1 is not None and num2 is not None:
+    result = num1 % num2
+
+
+
+
